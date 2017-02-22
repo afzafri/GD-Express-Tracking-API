7804d72ca68d3017a69fbb8510ad97f41398ee88
Afif Zafri <afzafri@gmail.com>
2017-02-22 21:07:33 +0800

    First commit

---
+++ api.php
@@ -0,0 +1,12 @@
+<?php
+
+/*  GD Express (GDEX) Tracking API created by Afif Zafri.
+    Tracking details are fetched directly from GDEX tracking website,
+    parse the content, and return JSON formatted string.
+    Please note that this is not the official API, this is actually just a "hack",
+    or workaround for implementing tracking feature in other project.
+    Usage: http://site.com/api.php?trackingNo=CODE , where CODE is your tracking number
+*/
+
+
+?>
