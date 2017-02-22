7655232abad42991103208b2e8040428c61c454d
Afif Zafri <afzafri@gmail.com>
2017-02-22 21:10:59 +0800

    fetch webpage data using cURL

--- api.php
+++ api.php
@@ -8,5 +8,37 @@
     Usage: http://site.com/api.php?trackingNo=CODE , where CODE is your tracking number
 */
 
+header("Access-Control-Allow-Origin: *"); # enable CORS
+
+if(isset($_GET['trackingNo']))
+{
+	$trackingNo = $_GET['trackingNo']; 
+
+    $url = "http://1.9.8.166/official/iframe/etracking2.php"; # url of gdex tracking website
+
+    # data for POST request
+    $postdata = http_build_query(
+        array(
+            'capture' => $trackingNo,
+            'redoc_gdex' => 'cnGdex',
+            'Submit' => 'Track',
+        )
+    );
+
+    # use cURL instead of file_get_contents(), this is because on some server, file_get_contents() cannot be used
+    # cURL also have more options and customizable
+    $ch = curl_init(); # initialize curl object
+    curl_setopt($ch, CURLOPT_URL, $url); # set url
+    curl_setopt($ch, CURLOPT_POST, 1); # set option for POST data
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); # set post data array
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+    $result = curl_exec($ch); # execute curl, fetch webpage content
+    $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # receive http response status
+    curl_close($ch);  # close curl
+
+    print_r($result);
+    echo "<br>";
+    print_r($httpstatus);
+}
 
 ?>
